d6c29765004a2a8ea6186f0f0b569891c9ad691f
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-04-13 22:29:31 -0400

    Fix route errors
    Route was retrieving form data from request instead of Json data
    and incorrectly structured Json was causing issues with the
    docker wrapper class.

--- app.py
+++ app.py
@@ -9,18 +9,26 @@ d = docker_ops.Docker()
 @app.route("/create-image", methods=['POST'])
 def create_image() -> dict[str, str]:
   # Read git repository link and environment info from body of POST request
-  git_repo_link = request.form['git_repo_link']
-  env_info = request.form['env_info']
+  data = request.json
+  git_repo_link = data['git_repo_link']
+  env_info = data['env_info']
   
   # Clone repository locally
   repo_id = uuid.uuid4()
   repo_path = docker_ops.clone_git_repo(git_repo_link, repo_id)
   
-  # Create image using given environment info
-  env_packages = env_info["build_command"] + env_info["packages"] + ["git"] + "gnupg"
+  # Create dockerfile using given environment info
+  env_packages = [
+    env_info["build_command"],
+    "git",
+    "gnupg"
+  ]
+ 
+  env_packages += env_info["packages"]
+  
   d.create_dockerfile(
     base_image=env_info["base_image"],
-    update_command="apk update",
+    update_command=env_info["update_command"],
     packages=env_packages,
     git_repo_dir=repo_path,
     start_command=env_info["start_command"]
@@ -29,7 +37,7 @@ def create_image() -> dict[str, str]:
   # Build image
   image = d.build_image("repos/{}".format(repo_id), repo_id)
   
-  # Send image id as response
+  # Send image info as response
   return {
     "image_id": image.id,
     "image_short_id": image.short_id,
